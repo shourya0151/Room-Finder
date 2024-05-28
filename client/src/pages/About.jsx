20bdfc322e97f42ba8d0292eb672e75cfbba16d4
Shourya Gupta <shouryaguptag11@gmail.com>
2024-05-28 16:06:30 +0530

    second commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About RoomConnect</h1>
+      <p className='mb-4 text-slate-700'>At <b>RoomConnect</b>, we understand the challenges of finding the perfect roommate and a comfortable living space. Whether you're a student seeking accommodation near your university, a young professional relocating to a new city, or someone simply looking for a compatible roommate, our platform is designed to make the process easier, safer, and more efficient.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is simple: to connect individuals in need of shared accommodation with compatible roommates in a hassle-free manner. We aim to create a vibrant community where users can find not just a place to live, but also like-minded individuals to share their living space and experiences with.
+      </p>
+      <p className='mb-4 text-slate-700'>At <b>RoomConnect</b>, we've streamlined the roommate search process. Users can create detailed profiles outlining their preferences, interests, and lifestyle choices. Our advanced search and matching algorithms then connect users with potential roommates based on compatibility factors such as location, budget, and lifestyle preferences.</p>
+      <h2>Why Choose us?</h2>
+      
+      <ol>
+        <li>Safety and Security: Your safety is our top priority. We employ robust security measures to protect your personal information and ensure a secure browsing experience.</li>
+        <li>User-Friendly Interface: Our website features an intuitive and user-friendly interface, making it easy for you to navigate, search for roommates, and manage your listings.</li>
+        <li>Diverse Community: We welcome individuals from all walks of life, fostering a diverse and inclusive community where everyone can find their ideal roommate match.</li>
+        <li>Reliable Support: Have questions or need assistance? Our dedicated support team is here to help you every step of the way. Whether you're having trouble creating your profile or need advice on finding the perfect roommate, we're just a message away.</li>
+      </ol>
+      
+
+    </div>
+  )
+}
